800d3e830a5595ed2f265e64441700657ba272d8
zenyrima <rimadamayanti69@gmail.com>
2021-04-03 14:19:02 +0800

    compile & run

---
+++ InputOutput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class InputOutput{
+ public static void main(String [] args){
+  Scanner scanner = new Scanner(System.in);
+  String nama;
+  System.out.print("Masukkan nama anda: ");
+  nama = scanner.nextLine();
+  System.out.println("Hai, "+nama);
+
+ }
+}
